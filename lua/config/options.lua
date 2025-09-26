58d550ce96ea036dfa0c39637c3e9f2201befcab
PeppermintSnow <peppermintsnow06@gmail.com>
2025-09-26 11:37:30 +0800

    refactor(options): modularize editor options

---
+++ lua/config/options.lua
@@ -0,0 +1,24 @@
+-- Keymap leader config
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Tab config
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- Line config
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+-- Display config
+vim.opt.signcolumn = "yes"
+
+-- Clipboard config
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+
+-- Performance config
+vim.opt.lazyredraw = true
