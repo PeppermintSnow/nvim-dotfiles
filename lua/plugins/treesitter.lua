22c129e7e13408f235f6479845fef01b8d8114dd
PeppermintSnow <peppermintsnow06@gmail.com>
2025-09-30 11:24:22 +0800

    feat: add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  opts = {
+    highlight = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsx",
+      "typescript",
+      "tsx",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "regex",
+      "toml",
+      "yaml",
+      "xml",
+      "php",
+    }
+  }
+}
