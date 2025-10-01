7622796d2e146988fa0b501eade5c2bd37af5480
PeppermintSnow <peppermintsnow06@gmail.com>
2025-10-01 08:09:28 +0800

    style: add color overrides to match tempus-warp terminal theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,14 @@ return {
 	priority = 1000,
 	config = function()
 		require('poimandres').setup({
-      disable_background = true
+      disable_background = true,
+      highlight_groups = {
+        CursorLine = { bg = "#003c39" },
+        CursorLineNr = { bg = "#003c39" },
+        CursorLineColumn = { bg = "#003c39" },
+
+        StatusLine = { bg = "#003c39", fg = "#89DDFF" }
+      }
     })
 		vim.cmd("colorscheme poimandres")
 	end
