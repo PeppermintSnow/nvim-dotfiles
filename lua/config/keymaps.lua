16824868bf47e14c6700a217248bf9fe10e423d0
PeppermintSnow <peppermintsnow06@gmail.com>
2025-10-06 21:11:20 +0800

    feat(keybinds): add ctrl hjkl motion in insert mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Telescope keybinds
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" } )
+
+-- Custom HJKL motion in insert mode
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
